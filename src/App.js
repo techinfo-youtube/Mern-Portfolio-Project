43bd517dcdb1e48665b93bc62ea571b499b356b8
Rasheed khan <rktechnicalplus@gmail.com>
2023-07-11 12:52:01 +0530

    about section
    complete code till about section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Layout from "./components/Layout/Layout";
+import About from "./pages/About/About";
 
 function App() {
   return (
     <>
       <Layout />
+      <div className="container">
+        <About />
+      </div>
     </>
   );
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
     <>
       <div className="container-fluid home-container">
         <div className="container home-content">
-          <h1>Hi I'm a</h1>
-          <h2>
+          <h2>Hi 👋 I'm a</h2>
+          <h1>
             <Typewriter
               options={{
                 strings: [
@@ -20,7 +20,7 @@ const Home = () => {
                 loop: true,
               }}
             />
-          </h2>
+          </h1>
           <div className="home-buttons">
             <button className="btn btn-hire">Hire Me</button>
             <a className="btn btn-cv" href={Resume} download="your_name.pdf">
