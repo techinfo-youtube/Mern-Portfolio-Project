dd1f2f4a701a25e57ea7f690753ac103bab38ffb
Rasheed khan <rktechnicalplus@gmail.com>
2023-07-24 15:26:03 +0530

    mobile navbar
    complete code till responsive navbar menu

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import WorkExp from "./pages/workExp/WorkExp";
 import ScrollToTop from "react-scroll-to-top";
 import { useTheme } from "./context/ThemeContext";
 import Tada from "react-reveal/Tada";
+import MobileNav from "./components/MobileNav/MobileNav";
 function App() {
   const [theme] = useTheme();
   return (
     <>
       <div id={theme}>
+        <MobileNav />
         <Layout />
         <div className="container">
           <About />
---
+++ client/src/components/MobileNav/MobileNav.js
@@ -0,0 +1,153 @@
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineMenuFold } from "react-icons/ai";
+import { Link } from "react-scroll";
+import {
+  FcAbout,
+  FcBiotech,
+  FcBusinessContact,
+  FcHome,
+  FcPortraitMode,
+  FcReadingEbook,
+  FcVideoProjector,
+} from "react-icons/fc";
+import "./MobileNav.css";
+const MobileNav = () => {
+  const [open, setOpen] = useState(false);
+
+  //handle open
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  // handle menu clicks
+  const handleMenuClick = () => {
+    setOpen(false);
+  };
+  return (
+    <>
+      <div className="mobile-nav">
+        <div className="mobile-nav-header">
+          {open ? (
+            <AiOutlineMenuFold
+              size={30}
+              className="mobile-nav-icon"
+              onClick={handleOpen}
+            />
+          ) : (
+            <GiHamburgerMenu
+              size={30}
+              className="mobile-nav-icon"
+              onClick={handleOpen}
+            />
+          )}
+
+          <span className="mobile-nav-title">My Portfolio App</span>
+        </div>
+        {open && (
+          <div className="mobile-nav-menu">
+            <div className="nav-items">
+              <div className="nav-item">
+                <div className="nav-link">
+                  <Link
+                    to="home"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcHome />
+                    Home
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcAbout />
+                    About
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="education"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcReadingEbook />
+                    Education
+                  </Link>
+                </div>
+
+                <div className="nav-link">
+                  <Link
+                    to="techstack"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcBiotech />
+                    Tech Stack
+                  </Link>
+                </div>
+
+                <div className="nav-link">
+                  <Link
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcVideoProjector />
+                    Projects
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="work"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcPortraitMode />
+                    Work Experince
+                  </Link>
+                </div>
+                <div className="nav-link">
+                  <Link
+                    to="contact"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={100}
+                    onClick={handleMenuClick}
+                  >
+                    <FcBusinessContact />
+                    Contact
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default MobileNav;
